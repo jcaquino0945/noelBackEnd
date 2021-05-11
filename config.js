a01d1e12271d145c28f5f9e1c7f74d6876e976be
jcaquino0945 <jcaquino0945@gmail.com>
2021-05-11 18:08:06 +0800

    set up backend with packages and connected to MongoDB Atlas @Noel

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '46a1483acbdad4b92f32f3331d262aff47e12e68b4739ac551f2bf4e4882df8fee186d83169c98b78591dc6c9c24ff53987f33af1ce90a1dccfa786d645b03c8',
+    'mongoUrl' : 'mongodb+srv://jcaquino0945:Pa$$w0rd@noelcluster.tqhja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
